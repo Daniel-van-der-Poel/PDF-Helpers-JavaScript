adeb3f1713af37842ab433d1a32c3c3444048ba1
Daniël <daniel.v.d.poel@gmail.com>
2016-11-16 21:34:42 +0100

    Added menu function

--- Functions for PDFs.js
+++ Functions for PDFs.js
@@ -1,11 +1,32 @@
+//Creates a popup menu, with possible submenus. The items parameter must be an array. Secondary arrays will create submenus, with the first item serving as the name of the submenu. E.g. createMenu(["new", "open", ["open recent", recent1, recent2, recent3], "save"])
+function createMenu(items, divider = ":") {
+  var menu = [];
+  function addItem(item, prefix) {
+    if (!Array.isArray(item)) {
+      return({cName: item, cReturn: prefix + divider + item});
+    } else {
+      var submenu = [];
+      for (var i = 1; i < item.length; i++) {
+        submenu.push(addItem(item[i], prefix + divider + item[0]));
+      }
+      return({cName: item[0], oSubMenu: submenu});
+    }
+  }
+  for (var i = 0; i < items.length; i++) {
+    menu.push(addItem(items[i], "Menu"));
+  }
+  return app.popUpMenuEx.apply(app, menu);
+}
+
+
 //Returns true if a field or other object exists, false if not.
 function fieldExists(target) {
     return (this.getField(target) !== null) ? true : false;
 }
 
 
-//Returns the value of a field. 
-function getValue (target) {
+//Returns the value of a field.
+function getValue(target) {
 	if (this.getField(target) !== null) {
 		return this.getField(target).value;
 	}
@@ -72,7 +93,7 @@ function setFont(target, fontname = font.Helv, size = 16, colorname = ["RGB", 0,
 
 
 //Sets the font, font size, font colour, and text alignment of all text fields, with optional  starting/ending field name selector. Example: massSetFont(font.HelvB, 12, color.red, "left", "customer")
-function massSetFont(fontname = font.Helv, size = 16, colorname = ["RGB", 0, 0, 0], align = "center", startname = "", endname = "") {
+function massSetFont(fontname = font.Helv, size = 9, colorname = ["RGB", 0, 0, 0], align = "left", startname = "", endname = "") {
     for (i = 0; i < this.numFields; i++) {
         var target = this.getNthFieldName(i);
         if (target.slice(0, startname.length) == startname && target.substr(-endname.length, endname.length) == endname && this.getField(target).type == "text") { //another option is "combobox"
@@ -81,13 +102,13 @@ function massSetFont(fontname = font.Helv, size = 16, colorname = ["RGB", 0, 0, 
             this.getField(target).alignment = align;
             this.getField(target).textColor = colorname;
             /* there are many more interesting field properties that can be changed this way, e.g.:
-            this.getField(target).richText = true; 
-            this.getField(target).defaultValue = ""; 
+            this.getField(target).richText = true;
+            this.getField(target).defaultValue = "";
             this.getField(target).doNotScroll = false;
             this.getField(target).doNotSpellCheck = false;
             this.getField(target).editable = false; (for combo-boxes)
             this.getField(target).fillColor = "T";
-            this.getField(target).userName = ""; 
+            this.getField(target).userName = "";
             */
         }
     }
